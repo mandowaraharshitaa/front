6fe858b51c86245700e33157b3f59280297bfe8a
mandowaraharshitaa <91795687+mandowaraharshitaa@users.noreply.github.com>
2024-12-01 13:57:11 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "https://server-ribs.onrender.com";
+export const server = "server-ribs.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
